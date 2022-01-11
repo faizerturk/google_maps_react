f31b66fe9ecc60f7976d6cb7d6f84b7e6b9bedf9
faizerturk <faizeeeturk@gmail.com>
2022-01-11 16:24:23 +0300

    İnitial code for map
    The map is shown here. App.js file is very basic but work properly.

--- src/App.js
+++ src/App.js
@@ -1,25 +1,24 @@
-import logo from './logo.svg';
-import './App.css';
+import React, {Component} from "react";
+import {Map, GoogleApiWrapper, InfoWindow, Marker} from "google-maps-react";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+const mapStyles = {
+  width: "100%", overflow: "hidden", position: "relative",
+};
+export class MapContainer extends Component {
+  render() {
+    return (
+      <Map
+        google={this.props.google}
+        zoom={10}
+        disableDefaultUI={true}
+        streetViewControl={true}
+        zoomControl={true}
+        mapTypeControl={true}
+      />
+    );
+  }
 }
-
-export default App;
+export default GoogleApiWrapper({
+  apiKey: 'AIzaSyCSuLaQoT1jvd4WjV6A2iYUO4x6SNcgnPE'
+})(MapContainer);
